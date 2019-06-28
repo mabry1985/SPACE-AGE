1d1c69815c357b668f9f75aa69a11e0a0e6df35f
mabry1985 <mabry1985@gmail.com>
2019-06-28 10:48:11 -0700

    begin spec for life expectancy

--- spec/spec.js
+++ spec/spec.js
@@ -3,21 +3,29 @@ import { Mercury, Venus, Mars, Jupiter } from '../src/space-age-calculator.js';
 describe('Earth age to age on other planets', function () {
   it('should return the inputted age * .24 rounded to nearest whole number', function () {
     let mercury = new Mercury;
-    expect(mercury.age(34)).toEqual(8.16);
+    expect(mercury.age(34)).toEqual(8);
   });
 
   it('should return the inputted age * .62 rounded to nearest whole number', function () {
     let venus = new Venus;
-    expect(venus.age(34)).toEqual(21.08);
+    expect(venus.age(34)).toEqual(21);
   });
 
   it('should return the inputted age * 1.88 rounded to nearest whole number', function () {
     let mars = new Mars;
-    expect(mars.age(34)).toEqual(63.92);
+    expect(mars.age(34)).toEqual(64);
   });
 
   it('should return the inputted age * 11.86 rounded to nearest whole number', function () {
     let jupiter = new Jupiter;
-    expect(jupiter.age(34)).toEqual(403.24);
+    expect(jupiter.age(34)).toEqual(403);
   });
 });
+
+
+describe('Life expectancy', function (){
+  it('should return your life')
+
+
+
+})
