c16b24726aef94f536c374685f10dab090dd9671
mabry1985 <mabry1985@gmail.com>
2019-06-30 10:59:35 -0700

    fix broken front-end code

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Mercury, Venus, Mars, Jupiter } from '../src/space-age-calculator.js';
 
 $(document).ready(function () {
-  const ageInput = parseInt($('.age').val());
   $('.mars').click(function () {
+    const ageInput = parseInt($('#ageInput').val());
     const marsOutput = new Mars;
-    ('.output').html(`<li> You are ${marsOutput.age(ageInput)} on Mars.</li>`);
+    $('.output').append(`<li> You are ${marsOutput.age(ageInput)} on Mars.</li>`);
   });
 });
